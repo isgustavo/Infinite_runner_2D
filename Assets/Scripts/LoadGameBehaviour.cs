a410cc1a3e6470691f8cd278e69b267cc8d3f0b8
isgustavo <hi.gustavo.oliveira@gmail.com>
2018-06-30 14:43:27 -0300

    Add load game scene

---
+++ Assets/Scripts/LoadGameBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameBehaviour : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Additive);
+    }
+}
